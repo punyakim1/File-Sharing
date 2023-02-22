4bf32f7d0a1f6262191f98c00a669e0dfb9c9dfb
punyakim1 <126011201+punyakim1@users.noreply.github.com>
2023-02-23 02:15:15 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -91,8 +91,8 @@ def fsub_button(client, message):
     if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="ᴊᴏɪɴ ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-                InlineKeyboardButton(text="ᴊᴏɪɴ ɢʀᴏᴜᴘ", url=client.invitelink2),
+                InlineKeyboardButton(text="𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘 📩ʟ", url=client.invitelink),
+                InlineKeyboardButton(text="𝗖𝗟𝗜𝗖𝗞 𝗛𝗘𝗥𝗘 📩", url=client.invitelink2),
             ],
         ]
         try:
